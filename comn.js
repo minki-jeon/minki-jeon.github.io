564ad0b0ead6d0904e280c6536618d73720c48ad
minki-jeon <minki.jeon.study@gmail.com>
2024-12-09 17:56:35 +0900

    add comn-header

---
+++ comn.js
@@ -0,0 +1,25 @@
+
+//* include Html-file load (header, footer)
+(function(){
+    function includeHtml() {
+        const includeTarget = document.querySelectorAll('.incFile');
+        includeTarget.forEach(function(el, idx) {
+            const targetFile = el.dataset.includeFile;
+            if(targetFile){
+                let xhttp = new XMLHttpRequest();
+            
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState === XMLHttpRequest.DONE) {
+                        this.status === 200 ? (el.innerHTML = this.responseText) : null
+                        this.status === 404 ? (el.innerHTML = 'include not found.') : null
+                    }
+                }
+                xhttp.open('GET', targetFile, true);
+                xhttp.send();
+                return;
+            }
+        });
+    };
+
+    includeHtml();
+})();
