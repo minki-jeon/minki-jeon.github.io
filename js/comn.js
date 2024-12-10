b99cde0ebe6206309de93ad223dd1cf2a52290d8
minki-jeon <minki.jeon.study@gmail.com>
2024-12-10 19:17:56 +0900

    드롭다운 스크립트 수정

--- js/comn.js
+++ js/comn.js
@@ -1,6 +1,28 @@
 
-//* include Html-file load (header, footer)
-//* Local 에서 동작을 할 수 없으므로 서비스용에서 사용
+  /**
+   * (/assets/js/main.js) Toggle mobile nav dropdowns
+   * 메뉴 텍스트에 드롭다운 이벤트 적용
+   */
+  //* TODO. a#services 에 onclick 으로 동작 방식 변경
+  document.querySelectorAll('.navmenu .dropdown-menu-test').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+    
+      e.preventDefault();
+    //   this.parentNode.classList.toggle('active');
+    //   this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+    
+      this.classList.toggle('active');
+      this.nextElementSibling.classList.toggle('dropdown-active');
+      e.stopImmediatePropagation();
+
+    });
+  });
+
+
+/** 
+ * include Html-file load (header, footer, nav)
+ * Local 에서 동작을 할 수 없으므로 서비스용에서 사용
+ */
 (function(){
     function includeHtml() {
         const includeTarget = document.querySelectorAll('.incFile');
